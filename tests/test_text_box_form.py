1b29eaa26ff5299c354f47417f88b283dad78ee9
Viacheslav Suchkov <iexploudi@gmail.com>
2025-06-27 00:13:44 +0400

    update test

--- tests/test_text_box_form.py
+++ tests/test_text_box_form.py
@@ -1,11 +1,26 @@
+import allure
+import pytest
+from allure_commons.types import Severity
+
 from demoqa.model.pajes.text_box_form import TextBoxPage
+from demoqa.utils.functions import valid_info
+from tests_data.person_data import test_cases
 from tests_data.user_info import User
-from selene import browser
 
 
+@allure.id("01_test_text_box")
+@allure.tag("web")
+@allure.severity(Severity.BLOCKER)
+@allure.label("owner", "vssuchkov")
+@allure.feature("Заполнение формы пользователя")
+@allure.story("Формы регистрации")
+@allure.link("https://demoqa.com/text-box", name="Testing url")
+@pytest.mark.parametrize("fio, mail, curr_address, per_address", test_cases)
+def test_text_box(setup_browser, fio, mail, curr_address, per_address):
+    user = User(fio, mail, curr_address, per_address)
+    valid_user_info = valid_info(user)
 
-def test_text_box():
-    text_box_page = TextBoxPage(browser)
+    text_box_page = TextBoxPage(setup_browser)
     text_box_page.open_page()
-    text_box_page.fill_text_box_form()
-    print()
+    text_box_page.fill_text_box_form(user)
+    text_box_page.should_be_form_info(valid_user_info)
